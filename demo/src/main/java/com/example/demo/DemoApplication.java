7db231c48b598e097dc7c5f81dc7edf8d2af6a31
Karandeep Bisht <90975899+KarandeepBisht@users.noreply.github.com>
2026-03-12 15:23:38 +0530

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,18 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
+        int arr[] = {1, 2, 4, 8, 6};
+        int largest = Integer.MIN_VALUE;
+        int secLargest = Integer.MIN_VALUE;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > largest){
+                secLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secLargest && arr[i] != largest) {
+                secLargest = arr[i];
+            }
+        }
+        System.out.println(secLargest + " " + largest);
 	}
 
 }
